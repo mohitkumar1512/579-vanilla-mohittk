12950644b776ee5e363e2edb96452d889c846718
Mohit Kumar <mohitkumar@Mohits-MacBook-Air-5.local>
2025-12-10 16:28:26 -0500

    Initial vanilla app

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
+document.getElementById("themeBtn").onclick = () =>
+  document.body.classList.toggle("dark");
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
+const quotes = [
+  "Hello from Mohit!",
+  "My uniqname is mohittk!",
+  "SI 579 is a great course!",
+  "JavaScript is fun!",
+  "You can do it!",
+  "Keep pushing forward!",
+];
 
+document.getElementById("quoteBtn").onclick = () => {
+  document.getElementById("quoteBox").innerText =
+    quotes[Math.floor(Math.random() * quotes.length)];
+};
+
+document.getElementById("msg").oninput = (e) =>
+  document.getElementById("count").innerText = e.target.value.length;
+
+let clicks = 0;
+document.getElementById("counter").onclick = () => {
+  clicks++;
+  document.getElementById("num").innerText = clicks;
+};
